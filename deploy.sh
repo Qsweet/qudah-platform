73bb9a2bc4d783e998e9dec0a0caaf9706835f81
qudah <qudah1985@gmail.com>
2026-01-03 22:29:48 +0300

    chore: add docker configuration

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Pull latest changes
+echo "Pulling latest changes..."
+git pull origin master
+
+# Stop existing container
+echo "Stopping existing containers..."
+docker-compose down
+
+# Build and start new container
+echo "Building and starting new container..."
+docker-compose up -d --build
+
+# Clean up unused images
+echo "Cleaning up unused images..."
+docker image prune -f
+
+echo "Deployment complete!"
